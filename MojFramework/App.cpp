d3c0373cea0ce719dcbdad4f60570940bec749f4
ales2026 <ale.urankar@gmail.com>
2025-05-26 19:36:25 +0200

    Cube implementation

--- MojFramework/App.cpp
+++ MojFramework/App.cpp
@@ -22,13 +22,20 @@ void App::UpdateModel()
 }
 
 #include "CubeScreenTransformer.h"
+#include "Cube.h"
 void App::ComposeFrame()
 {
-	CubeScreenTransformer pst;
-	Vec3 v0 = { 0.0f,0.5f,0.0f };
-	Vec3 v1 = { 0.5f,-0.5f,0.0f };
-	Vec3 v2 = { -0.5f,-0.5f,0.0f };
-	gfx.DrawLine(pst.GetTransformed(v0), pst.GetTransformed(v1), Colors::White);
-	gfx.DrawLine(pst.GetTransformed(v1), pst.GetTransformed(v2), Colors::White);
-	gfx.DrawLine(pst.GetTransformed(v2), pst.GetTransformed(v0), Colors::White);
+	CubeScreenTransformer cst;
+	Cube c(1.0f);
+	auto lines = c.GetLines();
+	for (auto& v : lines.vertices)
+	{
+		cst.Transform(v);
+	}
+	for (auto i = lines.indices.cbegin(),
+		end = lines.indices.cend();
+		i != end; std::advance(i, 2))
+	{
+		gfx.DrawLine(lines.vertices[*i], lines.vertices[*std::next(i)], Colors::White);
+	}
 }
