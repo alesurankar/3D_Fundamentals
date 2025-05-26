d1d208fe8ce72a91681051bb2ae0a9a735da9ee2
ales2026 <ale.urankar@gmail.com>
2025-05-26 20:47:34 +0200

    add angle wrapping function and test

--- MojFramework/MyMath.h
+++ MojFramework/MyMath.h
@@ -1,11 +1,19 @@
 #pragma once
-
-#include <math.h>
+#include <cmath>
 
 constexpr float PI = 3.14159265f;
+constexpr double PI_D = 3.1415926535897932;
 
 template <typename T>
-inline auto sq( const T& x )
+inline auto sq(const T& x)
 {
-	return x * x;
+    return x * x;
+}
+
+template<typename T>
+inline T wrap_angle(T theta)
+{
+    const T two_pi = T(2.0) * T(PI_D);
+    const T modded = std::fmod(theta, two_pi);
+    return (modded > T(PI_D)) ? (modded - two_pi) : modded;
 }
