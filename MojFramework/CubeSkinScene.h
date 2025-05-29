59e61994a2a0f8cc3501b104c68cbe355547eb70
ales2026 <ale.urankar@gmail.com>
2025-05-29 06:19:32 +0200

    implemented programable pixel shader stage in pipeline, added textureeffect, adapted scene

--- MojFramework/CubeSkinScene.h
+++ MojFramework/CubeSkinScene.h
@@ -1,14 +1,16 @@
 #pragma once
-
 #include "Scene.h"
 #include "Cube.h"
 #include "Mat3.h"
 #include "Pipeline.h"
+#include "TextureEffect.h"
+
 
 // scene demonstrating skinned cube
 class CubeSkinScene : public Scene
 {
 public:
+	typedef Pipeline<TextureEffect> Pipeline;
 	typedef Pipeline::Vertex Vertex;
 public:
 	CubeSkinScene(Graphics& gfx, const std::wstring& filename)
@@ -17,7 +19,7 @@ public:
 		pipeline(gfx),
 		Scene("Textured Cube skinned using texture")
 	{
-		pipeline.BindTexture(filename);
+		pipeline.effect.ps.BindTexture(filename);
 	}
 	virtual void Update(Keyboard& kbd, Mouse& mouse, float dt) override
 	{
