cbf10e88202298dd0bedc8e0db2264d039900189
ales2026 <ale.urankar@gmail.com>
2025-05-30 19:48:28 +0200

    automated adjustment of geometry positioning

--- MojFramework/App.cpp
+++ MojFramework/App.cpp
@@ -18,7 +18,7 @@ App::App(MainWindow& wnd)
 	gfx(wnd)
 {
 	scenes.push_back(std::make_unique<GeometryFlatScene>(gfx,
-		IndexedTriangleList<GeometryFlatScene::Vertex>::Load("models\\bunny.obj")));
+		IndexedTriangleList<GeometryFlatScene::Vertex>::Load("models\\character_1.obj")));
 	scenes.push_back(std::make_unique<GeometryFlatScene>(gfx, Cube::GetPlain<GeometryFlatScene::Vertex>()));
 	scenes.push_back(std::make_unique<CubeFlatIndependentScene>(gfx));
 	scenes.push_back(std::make_unique<CubeSolidGeometryScene>(gfx));
